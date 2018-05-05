6dcd9db44ee080e5d589e4a4b2ba844a623b4980
richard-xu1 <38999465+richard-xu1@users.noreply.github.com>
2018-05-05 17:18:21 -0400

    Create gating_time_step.py

---
+++ gating_time_step.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy as sp
+import h_sodium_inactivation as h
+import m_sodium_activation as m
+import n_potassium_activation as n
+import parameters as par
+
+# timestep for the gating variables
+dt = par.dt
+dx = par.dx
+
+def TimeStep(s,type,v): #n is type = 1, m is type = 2, h is type = 3
+    #define alpha and beta functions for the gate type
+    def alpha(v):
+        if type == 1:  
+            return n.alpha(v)
+        elif type == 2:
+            return m.alpha(v)
+        else:
+            return h.alpha(v)
+    def beta(v):
+        if type == 1:  
+            return n.beta(v)
+        elif type == 2:
+            return m.beta(v)
+        else:
+            return h.beta(v)     
+    nx = v.size
+    invdt = np.full((nx,1),1/dt)
+    A = alpha(v)
+    B = beta(v)
+    C1 = invdt + A/2 + B/2
+    C2 = invdt - A/2 - B/2
+    return np.divide((np.multiply(s,C2) + A),C1)
+        
+  
