c3614dc169d3ddab099fb64fa1795b03ad523641
gkluhana <35476057+gkluhana@users.noreply.github.com>
2018-05-05 17:26:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,44 @@ import numpy as np
 import scipy as sp
 import parameters 
 
-s_k = np.ndarray(n)
-s_kHalf = np.ndarray(n)
+#Initialize Arrays
+
+M_k = np.ndarray(n)       #array stores gating variable m at time k
+M_kHalf = np.ndarray(n)   #array stores gating variable m at time k+1/2
+
+
+N_k = np.ndarray(n)       #array stores gating variable n at time k
+N_kHalf = np.ndarray(n)   #array stores gating variable n at time k+1/2
+
+H_k = np.ndarray(n)       #array stores gating variable m at time k
+H_kHalf = np.ndarray(n)   #array stores gating variable m at time k+1/2
+
 
 v_k = np.ndarray(n)
 v_kHalf = np.ndarray(n)
 
-gatingVarSolve(s_k,s_kHalf)
-tridiagonalsolve(a,b,c,d)
+a=np.ndarray(n)
+b=np.ndarray(n)
+C=np.ndarray(n)
+
+a,b,c = makeCoefficients()
+
+t_steps= T/dt
+k_steps= 2*t_steps
+for k in range (t_steps)
+  #Update gating variables  
+  N_k = gatingVarSolveH(N_k,1,v_kHalf)
+  M_k = gatingVarSolveM(M_k,2,v_kHalf)
+  H_k = gatingVarSolveN(H_k,3,v_kHalf)
+
+  d= makeD(v_k, v_khalf,M_khalf,N_khalf,H_khalf)
+  
+  v_k = tridiagonalSolve(a,b,c,d)
+ 
+  N_kHalf = gatingVarSolveH(N_kHalf,1,v_k)
+  M_kHalf = gatingVarSolveM(M_kHalf,2,v_k)
+  H_kHalf = gatingVarSolveN(H_kHalf,3,v_k)
+  
+  dHalf=makeD(v_khalf,v_k,M_k,N_k,H_k)
+  v_kHalf = tridiagonalSolve(a,b,c,dHalf)
+  
