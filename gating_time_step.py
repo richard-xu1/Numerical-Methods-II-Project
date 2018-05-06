3123842ba8e5a0f8ff4c798b1297515e1f871b3e
gkluhana <35476057+gkluhana@users.noreply.github.com>
2018-05-06 16:29:56 -0400

    Update gating_time_step.py

--- gating_time_step.py
+++ gating_time_step.py
@@ -11,6 +11,25 @@ dx = par.dx
 n = par.n
 invdt = np.full((nx,1),1/dt) #create a nx by 1 list full of 1/dt for element wise addition/subtraction in calculation c1 and c2
 
+def Initialize(v,type){
+    def alpha(v):
+            if type == 1:  
+                return n.alpha(v)
+            elif type == 2:
+                return m.alpha(v)
+            else:
+                return h.alpha(v)
+        def beta(v):
+            if type == 1:  
+                return n.beta(v)
+            elif type == 2:
+                return m.beta(v)
+            else:
+                return h.beta(v)
+    v = (divide(alpha(v),(alpha(v)+beta(v))
+    
+}
+
 def TimeStep(s,type,v): #n is type = 1, m is type = 2, h is type = 3
     #takes s_k and v_{k+1/2} and returns s_{k+1}  
     #define alpha and beta functions for the gate type
