1daa9293c667eb89c681cb3e32e9fed78aa37fe5
gkluhana <35476057+gkluhana@users.noreply.github.com>
2018-05-06 17:03:52 -0400

    Update gating_time_step.py

--- gating_time_step.py
+++ gating_time_step.py
@@ -8,41 +8,38 @@ import parameters as par
 # timestep for the gating variables
 dt = par.dt
 dx = par.dx
-n = par.n
-invdt = np.full((nx,1),1/dt) #create a nx by 1 list full of 1/dt for element wise addition/subtraction in calculation c1 and c2
+N = par.n
+invdt = np.full((N,1),1/dt) #create a nx by 1 list full of 1/dt for element wise addition/subtraction in calculation c1 and c2
 
-def Initialize(v,type):
+def Initialize(v,gtype):
     def alpha(v):
-            if type == 1:  
+            if gtype == 1:  
                 return n.alpha(v)
-            elif type == 2:
+            elif gtype == 2:
                 return m.alpha(v)
             else:
                 return h.alpha(v)
     def beta(v):
-        if type == 1:  
+        if gtype == 1:  
             return n.beta(v)
-        elif type == 2:
+        elif gtype == 2:
             return m.beta(v)
         else:
             return h.beta(v)
     v = (np.divide(alpha(v),(alpha(v)+beta(v))))
-    
-
 
 def TimeStep(s,gtype,v): #n is type = 1, m is type = 2, h is type = 3
     #takes s_k and v_{k+1/2} and returns s_{k+1}  
     #define alpha and beta functions for the gate type
-        if gtype == 1:  
-            A = n.alpha(v)
-            B = n.beta(v)
-        elif gtype == 2:
-            A = m.alpha(v)
-            B = m.beta(v)
-        else:
-            A = h.alpha(v)
-            B = h.beta(v)
-
+    if gtype == 1:  
+        A = n.alpha(v)                 #calculate and store alpha(v)
+        B = n.beta(v)                  #calculate and store beta(v)
+    elif gtype == 2:
+        A = m.alpha(v)                 #calculate and store alpha(v)
+        B = m.beta(v)                  #calculate and store beta(v)
+    else:
+        A = h.alpha(v)                 #calculate and store alpha(v)
+        B = h.beta(v)                  #calculate and store beta(v)
     C1 = invdt + A/2 + B/2
     C2 = invdt - A/2 - B/2
     return np.divide((np.multiply(s,C2) + A),C1)
