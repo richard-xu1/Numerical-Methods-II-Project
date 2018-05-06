9a3ff433e9414ab8e929f803ffb1ba3daaf2093a
gkluhana <35476057+gkluhana@users.noreply.github.com>
2018-05-06 17:04:19 -0400

    Update Coefficients.py

--- Coefficients.py
+++ Coefficients.py
@@ -1,36 +1,37 @@
 import numpy as np
 import scipy as sp
-import parameters par
+import parameters as par
 
 def makeCoefficients(a,b,c):
-r = par.r
-rho = par.rho
-cn = par.CN
-cm = par.CM
-dx= par.dx
-dt= par.dt
-r1 = par.r1
-r2 = par.r2
+	r = par.r
+	rho = par.rho
+	cn = par.CN
+	cm = par.CM
+	dx= par.dx
+	dt= par.dt
+	r1 = par.r1
+	r2 = par.r2
+	n = par.n
 
-grid=par.grid
+	grid=par.grid
 
-#Interior Points
-for i in range(1,n-1)
-  if grid[i] == false
-    a[i]= -r/(4*rho*dx*dx)
-    b[i]= cn/dt + r/(2*rho*dx*dx) 
-    c[i]= -r/(4*rho*dx*dx)
-  else 
-    a[i]= -1./(2*dx*dx(r1+r2))
-    b[i]= cm/dt + 1./(dx*dx*(r1+r2))
-    c[i]= -1./(2*dx*dx(r1+r2))
-    
-#Boundary Points
-b[0] = 0
-c[0] = 0
+	#Interior Points
+	for i in range(1,n-1):
+	  if grid[i] == False:
+	    a[i]= -r/(4*rho*dx*dx)
+	    b[i]= cn/dt + r/(2*rho*dx*dx) 
+	    c[i]= -r/(4*rho*dx*dx)
+	  else:
+	    a[i]= -1./(2*dx*dx(r1+r2))
+	    b[i]= cm/dt + 1./(dx*dx*(r1+r2))
+	    c[i]= -1./(2*dx*dx(r1+r2))
+	    
+	#Boundary Points
+	b[0] = 0
+	c[0] = 0
 
-a[n-1] = 0 
-b[n-1] = 0
+	a[n-1] = 0 
+	b[n-1] = 0
 
   
 
