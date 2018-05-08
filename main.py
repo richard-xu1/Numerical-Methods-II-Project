ef7b27d30ee44643803e4652c8474265f25060f3
gkluhana <35476057+gkluhana@users.noreply.github.com>
2018-05-07 20:35:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,39 +8,25 @@ from rhs import maked
 
 n = par.n
 #Initialize Arrays
-
-M_k = np.ndarray(n)       #array stores gating variable m at time k
-M_kHalf = np.ndarray(n)   #array stores gating variable m at time k+1/2
-
-
-N_k = np.ndarray(n)       #array stores gating variable n at time k
-N_kHalf = np.ndarray(n)   #array stores gating variable n at time k+1/2
-
-H_k = np.ndarray(n)       #array stores gating variable m at time k
-H_kHalf = np.ndarray(n)   #array stores gating variable m at time k+1/2
+N_kh = np.ndarray(n)       #array stores gating variable n at time k+1/2
+M_kh = np.ndarray(n)       #array stores gating variable m at time k+1/2
+H_kh = np.ndarray(n)       #array stores gating variable m at time k+1/2
 
 
-v_k = np.ndarray(n)
-v_kHalf = np.ndarray(n)
+v_k = np.ndarray(n)        #array stires voltage at time k
 
 #Initial conditions
 #Initialize all vectors to zero
 for i in range(0,n):
     v_k[i] = 0.
-    v_kHalf[i] = 0.
-    N_k[i] = 0.
-    N_kHalf[i] = 0.
-    M_k[i] = 0.
-    M_kHalf[i] = 0.
-    H_k[i] = 0.
-    H_kHalf[i] = 0.
-#Initialize Gating variable vectors
-Initialize(N_k,1)
-Initialize(N_kHalf,1)
-Initialize(M_k,2)
-Initialize(M_kHalf,2)
-Initialize(H_k,3)
-Initialize(H_kHalf,3)
+    N_kh[i] = 0.
+    M_kh[i] = 0.
+    H_kh[i] = 0.
+    
+\#Initialize Gating variable vectors. Gating variables need to be initialized to rest potentials. page 11, eq 29,30
+Initialize(N_kh,1)
+Initialize(M_kh,2)
+Initialize(H_kh,3)
 
 #Initialize coefficient diagonals
 a=np.ndarray(n)
@@ -50,17 +36,17 @@ d=np.ndarray(n)
 
 #Compute coefficient diagonals
 makeCoefficients(a,c)
-t_steps = int (par.T / par.dt)
+t_steps = int (par.T / par.dt) #make this more precise
+
 #TimeStep
 for k in range (t_steps):
   #Update gating variables  
-  N_k = TimeStep(N_k,1,v_kHalf)
-  M_k = TimeStep(M_k,2,v_kHalf)
-  H_k = TimeStep(H_k,3,v_kHalf)
-  b= makeb(M_k,N_k,H_k)
-  d= maked(v_k,M_k,N_k,H_k)
-#   d = np.random.random(d.shape)
-  
-  v_k = tridiagonalSolve(a,b,c,d)
+  N_k = TimeStep(N_k,1,v_kHalf) #Timestep in n
+  M_k = TimeStep(M_k,2,v_kHalf) #Timestep in m
+  H_k = TimeStep(H_k,3,v_kHalf) #Timestep in h
+  b= makeb(b,N_k,M_k,,H_k)  #Update b
+  d= maked(d,v_k,N_k,M_k,H_k)   #Update d
+  d = np.random.random(d.shape)
+  v_k = tridiagonalSolve(a,b,c,d)  #Solve Tridiagonal system
   
 print v_k
