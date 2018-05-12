ab9dd080bc1fc850d6ed0cf7e828de93678a2af6
richard-xu1 <38999465+richard-xu1@users.noreply.github.com>
2018-05-12 18:22:54 -0400

    Update h_sodium_inactivation.py

--- h_sodium_inactivation.py
+++ h_sodium_inactivation.py
@@ -2,20 +2,12 @@ import numpy as np
 import scipy as sp
 # the equations for the sodium inactivation gating variable "h"
 
-# coefficients used in alpha/beta functions
-Aa = 0.1     #1/msec
-Ba = -10.    #mV
-Ca = 6.      #mvV
-Ab = 4.5     #1/msec
-Bb = 45.      #mV
-Cb = 10.      #mV   
-
 def alpha(v):
-    ah = Aa*(Ba-v)/(1 - np.exp((v-Ba)/Ca))
+    ah = 0.07*np.exp(-(v+70.)/20. )
     return ah
     
 def beta(v):
-    bh = Ab/( 1 + np.exp((Bb-v)/Cb))
+    bh = 1/(1 + np.exp(-(v+40.)/10.))
     return bh
     
     
