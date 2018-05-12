b2b63871cf7bf1a6e682f9be6648c08133a3bfe9
richard-xu1 <38999465+richard-xu1@users.noreply.github.com>
2018-05-12 04:01:58 -0400

    Update rhs_diagonal.py

--- rhs_diagonal.py
+++ rhs_diagonal.py
@@ -1,8 +1,20 @@
 #rhs.py builds the right hand side of the voltage equation given that the gating variables have been updated
 # 0: active cable internal, 1: passive internal, 2: active to passive
 # 3: passive to active, 4: start point, 5: end point
+import numpy as np
+import scipy as sp
+import parameters as par
 
-
+grid = par.grid
+CN = par.CN
+CM = par.CM
+dxa = par.dxa
+dxp = par.dxp
+dt = par.dt
+ra = par.ra
+rp = par.rp
+rhoa = par.rhoA
+rhop = par.rhoP
 #rhs of interior nodes depends on, v_j^k, v_j+1^k, v_j-1^k, g_j^k+(^(k1/2), E_j^k+(1/2)
 #g_j^k+(1/2) Can be computed from the gating variables , p8, eq 20
 #E_j^k+(1/2) can be computed from the gating variables , p8 ,eq 21
@@ -17,7 +29,12 @@
 #j=n-1: Atilde_n-1, gtilde_n-1^k+(1/2),Etilde_n-1^k+(1/2) v_n-1^k, v_n-2^k
 
 #maked would need the updated gating variables, and other parameters to compute the RHS coefficient
-def maked(m,n,h)
+def maked(m,n,h):
 
 # makeb updates the coefficients of the main diagonal since it's a function of the gating variables
-def makeb(b,n,m,h)
+def makeb(g):
+    b = [0]*n
+    for i in range(n):
+        if grid[i] == 0:
+            b[i] = (CN/dt + g[i]/2. + ra/(2*rhoa*dxa**2))
+            
