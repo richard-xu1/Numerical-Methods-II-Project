2769ac3c94fb0a92262602d8e431399d06ee1ae4
richard-xu1 <38999465+richard-xu1@users.noreply.github.com>
2018-05-12 18:27:00 -0400

    Update m_sodium_activation.py

--- m_sodium_activation.py
+++ m_sodium_activation.py
@@ -2,18 +2,10 @@ import numpy as np
 import scipy as sp
 # the equations for the sodium activation variable "m"
 
-# coefficients used in alpha/beta functions
-Aa = .36     #1/msec
-Ba = 22.    #mV
-Ca = 3.      #mvV
-Ab = .4     #1/msec
-Bb = 13.      #mV
-Cb = 20.     #mV   
-
 def alpha(v):
-    am = Aa*(v-Ba)/(1 - np.exp((Ba-v)/Ca))
+    am = ((v + 45.)/10.)/(1 - np.exp(-(v+45.)/10.))
     return am
     
 def beta(v):
-    bm = Ab*(Bb - v)/( 1 - np.exp((v-Bb)/Cb))
+    bm = 4.*np.exp(-(v+70.)/18.)
     return bm
