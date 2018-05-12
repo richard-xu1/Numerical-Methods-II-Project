40e5785f93fb00653ad125159cc287daff609590
gkluhana <35476057+gkluhana@users.noreply.github.com>
2018-05-12 01:33:04 -0400

    Update Coefficients.py

--- Coefficients.py
+++ Coefficients.py
@@ -9,36 +9,33 @@ def makeCoefficients(a,c):
     ra = par.ra
     rp = par.rp
     rhoa = par.rhoA
-	rohp = par.rhoA
-	cn = par.CN
-	cm = par.CM
-	dxa = par.dxa
-	dxp = par.dxp
-	n = par.n
-	grid=par.grid
+    rohp = par.rhoA
+    cn = par.CN
+    cm = par.CM
+    dxa = par.dxa
+    dxp = par.dxp
+    n = par.n
+    grid=par.grid  
 
-	#Interior Points
-	for i in range(1,n-1):
-	# 0: active cable internal, 1: passive internal, 2: active to passive
-	# 3: passive to active, 4: start point, 5: end point	
-	    if grid[i] == 0:    #active internal
-	        a[i]= -ra/(4*rhoa*dxa*dxa)
-	        c[i]= -ra/(4*rhoa*dxa*dxa)
+    #Interior Points
+    for i in range(1,n-1):
+    # 0: active cable internal, 1: passive internal, 2: active to passive
+    # 3: passive to active, 4: start point, 5: end point	
+        if grid[i] == 0:    #active internal
+            a[i]= -ra/(4*rhoa*dxa*dxa)
+            c[i]= -ra/(4*rhoa*dxa*dxa)
         elif grid[i] ==1:   #passive internal
-	        a[i]= -rp/(4*rhop*dxp*dxp)
-	        c[i]= -rp/(4*rhop*dxp*dxp)
+            a[i]= -rp/(4*rhop*dxp*dxp)
+            c[i]= -rp/(4*rhop*dxp*dxp)
         elif grid[i] ==2:   #active to passive
             a[i]= -(np.pi*ra)/(2*rhoa*dxa)
             c[i]= -(np.pi*rp)/(2*rhop*dxp)
         elif grid[i] ==3:   #passive to active
             a[i]= -(np.pi*rp)/(2*rhop*dxp)
             c[i]= -(np.pi*ra)/(2*rhoa*dxa)
-	    
-	#Boundary Points
+        
+    #Boundary Points
 
-	c[0] = -(np.pi*ra)/(2*rhoa*dxa)
-	a[n-1] = -(np.pi*ra)/(2*rhoa*dxa)
-
-  
-
+    c[0] = -(np.pi*ra)/(2*rhoa*dxa)
+    a[n-1] = -(np.pi*ra)/(2*rhoa*dxa)
 
