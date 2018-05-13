55a400d89b90bf7d85001daaac499f211cd51fec
richard-xu1 <38999465+richard-xu1@users.noreply.github.com>
2018-05-13 17:53:33 -0400

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -4,27 +4,27 @@ import numpy as np
 caseNumber = 1 #{Case #1: Unmyelenated , Case #2: Myelenated}
 
 #For Case #2
-numOfNor = 1                            #Number of Nodes of Ranvier excluding starting and end Nodes
+numOfNor = 20                            #Number of Nodes of Ranvier excluding starting and end Nodes
 mylSections = numOfNor + 1               #number of myelinated sections
-nodeLen = .0001                             # node of ranvier length in cm
+nodeLen = .002                             # node of ranvier length in cm
 myelinLen = 1.                               #length of myelinated section in cm
 
-norPoints = 10									         #Node of Ranvier Grid Points
+norPoints = 20									         #Node of Ranvier Grid Points
 MylNorRatio = myelinLen/nodeLen                              # Myelin length to Node length ratio
-mylPoints = 1000							# Myelin section Grid Points
+mylPoints = 20							# Myelin section Grid Points
 n = mylSections*mylPoints + (numOfNor + 2)*norPoints                       #Total points in Linear Cable 
 cableLength = nodeLen*(numOfNor + 2) + MylNorRatio*mylSections  #total Length of cable in cm
 dxa = nodeLen/norPoints                #dxa grid size on active cable
 dxp = myelinLen/mylPoints              #dxp grid size on passive cable
 
 #Physical Parameters
-ra = 0.001			  # active cable radius in cm
-rp = 0.001           #passive cable radius in cm
-rhoA = 35.4			  # active axoplasmic resistivity Ohm*cm
-rhoP = 35.4            # passive axoplasmic resistivity  Ohm*cm
+ra = 0.005			  # active cable radius in cm
+rp = 0.007           #passive cable radius in cm
+rhoA = 0.0354		  # active axoplasmic resistivity Ohm*cm
+rhoP = 0.0354            # passive axoplasmic resistivity  Ohm*cm
 CN = 1			  # nodal membrane capacitance in microfarads/cm^2
-gNA = 120                # active sodium gating constant   
-gK = 36                  # active potassium gating constant 
+gNA = 2400                # active sodium gating constant   
+gK = 200                  # active potassium gating constant 
 gL = 0.3                     # active leakage gating constant
 ratio=0.0001
 CM = 0.001*CN			  # myelinated membrane capacitance in microfards/cm^2
@@ -38,14 +38,14 @@ v_rest = -70            # mV
 ELP= -70
 
 T=1     
-dt=.00001
+dt=.002
 tsteps=int (T/dt)
 
 
 #Parameters for Injected Current
 t1 = 0.01
 t2 = 0.02
-i0 = 0.03          
+i0 = 50.          
 #Build Grid should create create arrays v[n],M[n], N[n], H[n] which store the value
 #of voltage and gating variables at time t_k.
 
