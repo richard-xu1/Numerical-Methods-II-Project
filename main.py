0cfea2137b1cbd7b71d4781dd1fe7a052c692da2
gkluhana <35476057+gkluhana@users.noreply.github.com>
2018-05-13 20:28:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -10,86 +10,95 @@ from Tridiagonal_Solver import tridiagonalSolve
 from rhs_diagonal import maked,makeb
 from gating_coefficients import Aj,Ae,calc_gate_coeff
 
+#Movieimport matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
 n = par.n
+x_axis = np.linspace(1,n,num=n,endpoint=True)
+print "x_axis is" +str(x_axis)
 v_rest = par.v_rest
 dt = par.dt
-#Initialize Arrays
-# N_kh = np.ndarray(n)       #array stores gating variable n at time k+1/2
-# M_kh = np.ndarray(n)       #array stores gating variable m at time k+1/2
-# H_kh = np.ndarray(n)       #array stores gating variable m at time k+1/2
+Tf = par.Tf
+T = par.T
+nf  = int(T/Tf)+1
 
 v_k = np.ndarray(n)        #array stires voltage at time k
-
 #Initial conditions
 #Initialize all vectors to zero
 for i in range(0,n):
   v_k[i] = v_rest
   
+
 #Initialize Gating variable vectors. Gating variables need to be initialized to rest potentials. page 11, eq 29,30
 N_kh = Initialize(v_k,1)
 M_kh = Initialize(v_k,2)
 H_kh = Initialize(v_k,3)
 
-# print "n is" + str(N_kh)
-# print "m is" + str(M_kh)
-# print "H is" + str(H_kh)
 
 
 #Initialize coefficient diagonals arrays
 
-start = time.time()
 a,c = makeCoefficients()
-duration = time.time() - start
-start=time.time()
-print"A Code time" + str(duration)
-#Compute coefficient diagonals
+
+vFrames = np.ndarray([ nf, n])
 
 t_steps = par.tsteps
+t    = 0.    #   time since the beginning of the simulation
+tf   = 0.    #   time since the beginning of the frame
 
-t = 0
 #TimeStep
 print "v0 is " + str(v_k)
-for k in range (100000):
-	start=time.time()
-	#Update gating variables
-	# print "Time is" + str(t)
-	N_kh = TimeStep(N_kh,1,v_k) #Timestep in n
-	duration = time.time() - start
-	start=time.time()
-	print"N Code time" + str(duration)
-	M_kh = TimeStep(M_kh,2,v_k) #Timestep in m
-	duration = time.time() - start
-	start=time.time()
-	print"M Code time" + str(duration)
-	H_kh = TimeStep(H_kh,3,v_k) #Timestep in h
-	duration = time.time() - start
-	start=time.time()
-	print"H Code time" + str(duration)
-	# print "n is" + str(N_kh)
-	# print "m is" + str(M_kh)
-	# print "H is" + str(H_kh)
-	g,E = calc_gate_coeff(N_kh,M_kh,H_kh)
-	duration = time.time() - start
-	start=time.time()
-	print"G Code time" + str(duration)
+frame = 0
+while t < T:
+    N_kh = TimeStep(N_kh,1,v_k) #Timestep in n
+    M_kh = TimeStep(M_kh,2,v_k) #Timestep in m
+    H_kh = TimeStep(H_kh,3,v_k) #Timestep in h
+    g,E = calc_gate_coeff(N_kh,M_kh,H_kh)		
+    b= makeb(g)                  #Update b
+    current= par.injectedCurrent(t+0.5*dt)      #compute current
+    d= maked(v_k,g,E,current)           #Update d
+    v_k = tridiagonalSolve(a,b,c,d)  #Solve Tridiagonal system
+    t = t + dt
+    tf= tf+ dt
+    print "t is" + str(t)
+    # print "v_k is"+str(v_k)
+    if tf > Tf:
+        print "saving frame no" +str(frame)
+        vFrames[frame, :] = v_k
+        frame = frame+1
+        tf=0.
 
-	
-	b= makeb(g)  #Update b
-	duration = time.time() - start
-	start=time.time()
-	print"B Code time" + str(duration)
 
-	current= par.injectedCurrent(t+0.5*dt)
+print "v is " + str(v_k)
 
-	d= maked(v_k,g,E,current)#Update d
-	duration = time.time() - start
-	start=time.time()
-	print"D Code time" + str(duration)
-	#   d = np.random.random(d.shape)
-	v_k = tridiagonalSolve(a,b,c,d)  #Solve Tridiagonal system
-	t = t + dt
-	if k%1000==0 :
-	  print "T is" +str(t)
-	  print "v is" +str(v_k)
-print "vk is " + str(v_k)
 
+fig = plt.figure()
+ax = plt.axes()
+line, = ax.plot([], [], lw=2)  
+
+def init():
+    line.set_data([], [])
+    return line,
+
+def animate(i): 
+    z = vFrames[i,:]
+    # print  "z is" + str(z)
+    t = Tf*i
+    titleString = 't=%8.2f ,dt=%8.4f' % (t,dt)
+    line.set_data(x_axis,z)
+    plt.title(titleString)
+    plt.xlim(0., n)
+    plt.ylim(-100,100)
+    # plt.show()
+    return line,  
+
+
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=nf)
+
+dpi = 200
+writer = animation.writers['ffmpeg'](fps=10)
+anim.save('test.mp4',writer=writer,dpi=dpi)
+
+plt.show()
