beba572bf93614a0e2f89231084b18d6d83d350c
gkluhana <35476057+gkluhana@users.noreply.github.com>
2018-05-14 22:34:36 -0400

    Add files via upload

--- driver_conv_std.py
+++ driver_conv_std.py
@@ -1,9 +1,9 @@
-import main
+import main_conv_std
 import numpy as np
 
-std1,timestamp1 = main.main(1)
-std2,timestamp2 = main.main(2)
-std4,timestamp4 = main.main(3)
+std1,timestamp1 = main_conv_std.main(1)
+std2,timestamp2 = main_conv_std.main(2)
+std4,timestamp4 = main_conv_std.main(3)
 
 
 conv = np.log2(np.divide(np.absolute(std4 - std2),np.absolute(std2 - std1))) 
